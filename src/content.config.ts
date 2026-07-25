2dfd3cf336dd8df2db0897762fa9e520672e063a
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-07-25 16:58:07 -0400

    set up first content collection and a utils file

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
+
+const blogCollection = defineCollection({
+  // type: "content",
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
