6a72abf83d759a673bd5814d38e9e2df0727b29c
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-07-31 15:35:22 -0400

    setup the nascar route and rendered a test post

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,6 @@ import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 
 const blogCollection = defineCollection({
-  // type: "content",
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
@@ -14,6 +13,19 @@ const blogCollection = defineCollection({
   }),
 });
 
+const nascarCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/nascar" }),
+  schema: ({ image }) =>
+    z.object({
+      raceTitle: z.string(),
+      raceDate: z.date(),
+      mainImage: image(),
+      mainImageAlt: z.string(),
+      tags: z.array(z.string()),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  nascar: nascarCollection,
 };
---
+++ src/content/nascar/test-nascar-post.md
@@ -0,0 +1,11 @@
+---
+raceTitle: "This is a test title"
+raceDate: 1900-01-02
+mainImage: "/src/images/nascar/5-31-2014_dover.webp"
+mainImageAlt: "Image of a signed ticket from the race"
+tags: ["winner", "track", "year"]
+---
+
+Here are some words. I am going to have to write a bunch more.
+
+![heres some alt text](../../images/nascar/DSC04458.JPG)
