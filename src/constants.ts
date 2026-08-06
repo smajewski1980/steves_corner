9cb867a9116126c70769dd7d7b714f09ef47fde6
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-08-06 10:36:17 -0400

    added a bunch of basic posts so i could see cards on the nascar page

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "TechPeople Blog";
-export const SITE_DESCRIPTION = "Articles, Stories & Tutorials For Tech People";
+export const SITE_TITLE = "Steve's Corner Of The Internet";
+export const SITE_DESCRIPTION = "A place to share my stuff";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
+export const ARTICLES_PER_PAGE = 9;
