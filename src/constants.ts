1b177bc36193474bb12a776ca27eda40152a7bb8
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-08-22 21:59:29 -0400

    added parallel lines test pressing to music items

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const SITE_TITLE = "Steve's Corner Of The Internet";
 export const SITE_DESCRIPTION = "A place to share my stuff";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const NASCAR_CARDS_PER_PAGE = 9;
-export const MUSIC_CARDS_PER_PAGE = 8;
+export const MUSIC_CARDS_PER_PAGE = 12;
 export const HA_ATTRIBUTION = "Imaged by Heritage Auctions, HA.com";
